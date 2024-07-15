8e1ac83ff3f044677785d9cadf9364ed038e1f5e
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2024-07-15 14:27:40 -0700

    added input validation for events

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -18,6 +18,26 @@ export async function POST(request: Request) {
   const cookieStore = cookies()
   const supabase = createServerClient(cookieStore)
   const body = await request.json()
+
+  if (!body.name) {
+    return NextResponse.json({ message: 'Name is required' }, { status: 400 })
+  } else if (body.name.length == 0) {
+    return NextResponse.json(
+      { message: 'Name must not be empty' },
+      { status: 400 },
+    )
+  } else if (body.name.length > 40) {
+    return NextResponse.json(
+      { message: 'Name must be less than 40 characters' },
+      { status: 400 },
+    )
+  } else if (typeof body.name != 'string') {
+    return NextResponse.json(
+      { message: 'Name must be a string' },
+      { status: 400 },
+    )
+  }
+
   const { data, error } = await supabase.from('Users').insert([body]).select()
 
   if (error) {
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -34,6 +34,43 @@ export default function Page() {
     getData()
   }, [])
 
+  const insertEvent = (
+    title: string,
+    description: string,
+    startTime: string,
+    endTime: string,
+    location: string,
+    timezone: string,
+    mode: string,
+    config: JSON,
+    creator: string,
+  ) => {
+    fetch('/api/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        startTime: startTime,
+        endTime: endTime,
+        location: location,
+        timezone: timezone,
+        mode: mode,
+        config: config,
+        creator: creator,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setUserData((prevData) => ({ ...prevData, [name]: value }))
@@ -41,58 +78,19 @@ export default function Page() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const { data: insertedData, error } = await supabase
-      .from('Users')
-      .insert([userData])
-      .select()
-    // fetch('/api/users', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     name: "Test user2"
-    //   })
-    // })
-
-    // fetch('/api/events', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     title: "Test inserting event with API",
-    //     description: "API insert",
-    //     startTime: "2022-01-01T00:00:00",
-    //     endTime: "2022-01-01T01:00:00",
-    //     location: "API",
-    //     timezone: "2022-01-01T01:00:00+00",
-    //     isWeekly: true,
-    //     config: {
-    //       "friday": "True",
-    //       "monday": "False",
-    //       "sunday": "True",
-    //       "tuesday": "True",
-    //       "saturday": "False",
-    //       "thursday": "False",
-    //       "wednesday": "False"
-    //     },
-    //     creator: "03db2d61-abc4-4f3a-8cc8-8995494787bf"
-    //   })
-    // })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch((error) => console.error('Error:', error));
-
-    if (error) {
-      setMessage(`Error inserting data: ${error.message}`)
-    } else {
-      setMessage('Data inserted: ' + JSON.stringify(insertedData[0].name))
-      setUserData({ name: '' })
-      setUsers((prevUsers) =>
-        prevUsers ? [...prevUsers, userData] : [userData],
-      )
-    }
+    insertEvent(
+      'test',
+      'test desc',
+      '2022-01-01T00:00:00Z',
+      '2022-01-01T01:00:00Z',
+      'test location',
+      'UTC',
+      'weekly',
+      JSON.parse(
+        '{"monday": true, "tuesday": true, "wednesday": true, "thursday": true, "friday": true, "saturday": false, "sunday": false}',
+      ),
+      '9e33186f-95db-4385-a974-ee38c8e07547',
+    )
   }
 
   return (
