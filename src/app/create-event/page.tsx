d8aaa3ea3cfb61272c7f832cebb9b53774e65aef
AlanaDubie <alanadubie922@gmail.com>
2024-07-19 11:37:00 -0700

    removed importing test Board component (this will be added for right side panel later on)

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 
 import EventForm from '@/components/EventForm'
-import Board from '@/components/Board'
 
 export default function CreateEvent() {
   const [title, setTitle] = useState('')
