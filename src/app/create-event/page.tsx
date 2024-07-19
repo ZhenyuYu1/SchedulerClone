d88a919b506a5d4f795fa4d41236f2120289eae1
AlanaDubie <alanadubie922@gmail.com>
2024-07-19 11:23:52 -0700

    Fixed container stylings in page.tsx and EventForm.tsx component

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 
 import EventForm from '@/components/EventForm'
+import Board from '@/components/Board'
 
 export default function CreateEvent() {
   const [title, setTitle] = useState('')
@@ -23,22 +24,35 @@ export default function CreateEvent() {
   }
 
   return (
-    <EventForm
-      title={title}
-      setTitle={setTitle}
-      description={description}
-      setDescription={setDescription}
-      location={location}
-      setLocation={setLocation}
-      earliestTime={earliestTime}
-      setEarliestTime={setEarliestTime}
-      latestTime={latestTime}
-      setLatestTime={setLatestTime}
-      daysOfWeek={daysOfWeek}
-      setDaysOfWeek={setDaysOfWeek}
-      timezone={timezone}
-      setTimezone={setTimezone}
-      handleSubmit={handleSubmit}
-    />
+    <div //main screen
+      className="flex min-h-screen w-full flex-col md:flex-row"
+    >
+      <section //Left side container
+        className="h-full w-full p-10 md:w-2/5"
+      >
+        <EventForm
+          title={title}
+          setTitle={setTitle}
+          description={description}
+          setDescription={setDescription}
+          location={location}
+          setLocation={setLocation}
+          earliestTime={earliestTime}
+          setEarliestTime={setEarliestTime}
+          latestTime={latestTime}
+          setLatestTime={setLatestTime}
+          daysOfWeek={daysOfWeek}
+          setDaysOfWeek={setDaysOfWeek}
+          timezone={timezone}
+          setTimezone={setTimezone}
+          handleSubmit={handleSubmit}
+        />
+      </section>
+      <section //Right side container
+        className="w-full p-4 md:w-3/5 md:p-10"
+      >
+        <p>Right Panel</p>
+      </section>
+    </div>
   )
 }
