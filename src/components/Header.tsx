f3fce114f9b1edd3d53664bdb7b9936573750733
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2024-08-01 11:48:32 -0700

    fixed src for calendar icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ export default function Header() {
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex text-xl font-bold">
           <Image
-            src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678116-calendar-512.png"
-            width="10"
-            height="10"
+            src="/CalendarIcon.png"
+            width="1000"
+            height="1000"
             alt="calendar"
             className="inline-block h-8 w-8"
           />
