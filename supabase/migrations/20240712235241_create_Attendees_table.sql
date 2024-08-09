a6edbfbe956cac385243502d9a4903189e12faf3
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2024-08-09 14:01:37 -0700

    added seed data for attendees timesegments

--- supabase/migrations/20240712235241_create_Attendees_table.sql
+++ supabase/migrations/20240712235241_create_Attendees_table.sql
@@ -1,6 +1,7 @@
 create table
 Attendees (
 id uuid primary key default gen_random_uuid(),
-eventID float,
-attendee uuid references Users(id)
+eventID uuid references Events(id),
+attendee uuid references Users(id),
+timesegments json
 );
